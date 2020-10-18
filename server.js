663763dee8a08b9d5fed49b2f46a261ffcb3c150
Owais Islam <owaisislam@gmail.com>
2020-10-18 13:35:02 -0500

    added addRole

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const {
+    throwError
+} = require('rxjs');
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'overlord',
-    database: 'tracker'
+    database: 'employee-tracker'
 })
-
 
 function mainMenu() {
     inquirer.prompt([{
             name: 'mainMenu',
             type: 'list',
             message: "Please select an option: ",
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add an employee', 'Update an employee role', 'Exit']
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
         }])
         .then(response => {
             switch (response.mainMenu) {
@@ -30,6 +32,9 @@ function mainMenu() {
                     break;
                 case 'Add a department':
                     addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
                     break;
                 case 'Add an employee':
                     addEmployee();
@@ -85,7 +90,7 @@ function addDepartment() {
             message: 'Enter the department name: '
         }])
         .then(response => {
-            connection.query("INSERT INTO department(id, name) VALUES (?,?)", [10, response.name], (error, result) => {
+            connection.query('INSERT INTO department(name) VALUES (?)', [response.name], (error, result) => {
                 if (error) throw error;
             })
 
@@ -93,12 +98,101 @@ function addDepartment() {
         })
 }
 
-function addEmployee() {
+function addRole() {
+    inquirer.prompt([{
+                name: 'name',
+                type: 'input',
+                message: 'Enter the role name: '
+            },
+            {
+                name: 'salary',
+                type: 'number',
+                message: 'Enter the salary: '
+            },
+            {
+                name: 'department',
+                type: 'list',
+                message: 'Select the department:',
+                choices: getDepartments()
+            }
+        ])
+        .then(response => {
 
+            var responseID = 0;
+
+            connection.query('SELECT id FROM department WHERE name = ?', [response.department], (error, result) => {
+                if (error) throw error;
+                result.forEach(id => {
+                    responseID = id.id;
+                })
+            })
+
+
+            connection.query('INSERT INTO role SET ?', {
+                title: response.name,
+                salary: response.salary,
+                department_id: responseID
+            }, (error, result) => {
+                if (error) throw error;
+            })
+
+            viewRoles();
+        })
+}
+
+function addEmployee() {
+    inquirer.prompt([{
+                name: 'firstName',
+                type: 'input',
+                message: 'Enter the employee first name: '
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: 'Enter the employee last name: '
+            }
+        ])
+        .then(response => {
+            connection.query('INSERT INTO employee(first_name, last_name) VALUES (?,?)', [response.firstName, response.lastName], (error, result) => {
+                if (error) throw error;
+            })
+
+            viewDepartments();
+        })
 }
 
 function updateEmployee() {
 
+}
+
+function getDepartments() {
+    let departments = [];
+    connection.query('SELECT name FROM department', (error, response) => {
+        if (error) throw error;
+
+        response.forEach(department => {
+            departments.push(department.name);
+            // console.log(departments);
+        })
+        // console.log(departments);
+    })
+
+    // console.log(departments);
+
+    return departments;
+}
+
+function getRoles() {
+    let roles = [];
+    connection.query('SELECT title FROM role', (error, response) => {
+        if (error) throw error;
+
+        response.forEach(role => {
+            roless.push(role.name);
+        })
+    })
+
+    return roles;
 }
 
 
